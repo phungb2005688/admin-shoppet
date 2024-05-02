1f737614bb0f033d3d62b6df125fe5e8c8709a1b
phungb2005688 <122144847+phungb2005688@users.noreply.github.com>
2024-05-02 17:27:26 +0700

    update

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,33 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const token = localStorage.getItem("token");
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  userInfo: localStorage.getItem("userInfo")
-    ? JSON.parse(localStorage.getItem("userInfo"))
+  userInfo: localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
     : null,
-  token: token ? token : null,
 };
 
 const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
     setCredentials: (state, action) => {
-      if (action.payload.user && action.payload.token) {
-        state.userInfo = action.payload.user;
-        state.token = action.payload.token;
-        localStorage.setItem('userInfo', JSON.stringify(action.payload.user));
-        localStorage.setItem('token', action.payload.token);
-      } else {
-        console.error('Invalid user data or token:', action.payload);
-      }
+      state.userInfo = action.payload;
+      localStorage.setItem('userInfo', JSON.stringify(action.payload));
     },
     logout: (state, action) => {
       state.userInfo = null;
       // NOTE: here we need to also remove the cart from storage so the next
       // logged in user doesn't inherit the previous users cart and shipping
-      state.token = null; // Xóa token
       localStorage.clear();
     },
   },
